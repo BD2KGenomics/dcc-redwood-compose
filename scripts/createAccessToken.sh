c6a89485ff7df4c78f0320d4c3082a6fb3052d21
Benjamin Ran <benjaminran2@gmail.com>
2017-01-23 17:23:23 -0800

    renamed containers, disabled remote debug forwarding, and updated storage-server config

--- scripts/createAccessToken.sh
+++ scripts/createAccessToken.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-user=beni
+user=testuser
 
-docker exec -it ucsc-auth-server curl -k -XPUT https://localhost:8543/admin/scopes/$user -u admin:secret -d"s3.upload s3.download"
-curl -k https://localhost:8443/oauth/token -H "Accept: application/json" -dgrant_type=password -dusername=$user -dscope="s3.upload s3.download" -ddesc="test access token" -u mgmt:pass
+docker exec -it redwood-auth-server curl -XPUT http://localhost:8543/admin/scopes/$user -u admin:secret -d"s3.upload s3.download"
+docker exec -it redwood-auth-server curl http://localhost:8443/oauth/token -H "Accept: application/json" -dgrant_type=password -dusername=$user -dscope="s3.upload s3.download" -ddesc="test access token" -u mgmt:pass
